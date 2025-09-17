8ad5274ec689ab1c6a31df064306b4769abdb463
Magnus <huynhbuutu@gmail.com>
2025-09-17 23:55:36 +0700

    add missing file

---
+++ BarcodePrintLabel/Views/LabelTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using BarcodePrintLabel.ViewModels;
+
+namespace BarcodePrintLabel.Views
+{
+    public class LabelTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate UnitTemplate { get; set; }
+        public DataTemplate BoxTemplate { get; set; }
+        public DataTemplate CartonTemplate { get; set; }
+        public DataTemplate FullTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is LabelPreviewDialogViewModel vm)
+            {
+                return vm.SelectedLabelType switch
+                {
+                    "Unit" => UnitTemplate,
+                    "Box" => BoxTemplate,
+                    "Carton" => CartonTemplate,
+                    "Full" => FullTemplate,
+                    _ => base.SelectTemplate(item, container),
+                };
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
